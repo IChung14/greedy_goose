8f79a06f49c39dcac99f5ed33a685e21d227fb27
ichung14 <34329462+IChung14@users.noreply.github.com>
2022-05-19 12:17:19 -0400

    example code implemented

--- app/src/main/java/com/example/greedygoose/MainActivity.kt
+++ app/src/main/java/com/example/greedygoose/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.greedygoose
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkOverlayPermission()
+        startService()
+    }
+
+    // method for starting the service
+    private fun startService() {
+        // check if the user has already granted
+        // the Draw over other apps permission
+        if (Settings.canDrawOverlays(this)) {
+            // start the service based on the android version
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(Intent(this, ForegroundService::class.java))
+            } else {
+                startService(Intent(this, ForegroundService::class.java))
+            }
+        }
+    }
+
+    // method to ask user to grant the Overlay permission
+    private fun checkOverlayPermission() {
+        if (!Settings.canDrawOverlays(this)) {
+            // send user to the device settings
+            val myIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+            startActivity(myIntent)
+        }
+    }
+
+    // check for permission again when user grants it from
+    // the device settings, and start the service
+    override fun onResume() {
+        super.onResume()
+        startService()
     }
 }
