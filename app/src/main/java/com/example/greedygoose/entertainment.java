905ec713db586326f1e6ac7bccd5a2dfb54e9c95
Naomy Sabo <naomysabo@Naomys-MacBook-Pro.local>
2022-06-08 19:45:01 -0400

    Redirect to entertainmentpage on button click

--- app/src/main/java/com/example/greedygoose/entertainment.java
+++ app/src/main/java/com/example/greedygoose/entertainment.java
@@ -1,6 +1,18 @@
 package com.example.greedygoose;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+
+import androidx.annotation.Nullable;
+
 
 public class entertainment extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.entertainment_fragment);
+    }
+
+
 }
