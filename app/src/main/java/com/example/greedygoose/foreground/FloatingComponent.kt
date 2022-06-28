6020ea22028957c991566081d2346bf7fca94417
ichung14 <34329462+IChung14@users.noreply.github.com>
2022-06-28 12:42:07 -0400

    code refine. setting image is now done by method chaining

--- app/src/main/java/com/example/greedygoose/foreground/FloatingComponent.kt
+++ app/src/main/java/com/example/greedygoose/foreground/FloatingComponent.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.os.Bundle
 import android.os.ResultReceiver
 import android.view.WindowManager
+import androidx.annotation.DrawableRes
+import com.example.greedygoose.R
 import com.example.greedygoose.foreground.movementModule.MovementModule
 import com.example.greedygoose.foreground.ui.FloatingWindowModule
 import kotlinx.coroutines.Job
@@ -17,12 +19,14 @@ import kotlinx.coroutines.launch
 class FloatingComponent(context: Context) {
     var receiver: ResultReceiver? = null
     var windowModule = FloatingWindowModule(context)
+    @DrawableRes private var imgRes: Int? = null
     private var movementModule: MovementModule? = null
     private var moduleHelper: ((FloatingWindowModule)->MovementModule)? = null
 
     fun build(): FloatingComponent {
         // creating a floating view
         windowModule.create()
+        windowModule.binding.gooseImg.setImageResource(R.drawable.egg_small)
         moduleHelper?.let {
             movementModule = it(windowModule)
             movementModule!!.run()
@@ -31,12 +35,13 @@ class FloatingComponent(context: Context) {
         return this
     }
 
+    fun setImageResource(@DrawableRes resId: Int): FloatingComponent{
+        imgRes = resId
+        return this
+    }
 
-    fun setWindowLayoutParams(x: Int, y: Int): FloatingComponent {
-        val layoutParams = windowModule.defaultParam()
-        layoutParams.x = x
-        layoutParams.y = y
-        windowModule.params = layoutParams
+    fun setWindowLayoutParams(params: WindowManager.LayoutParams): FloatingComponent {
+        windowModule.params = params
         return this
     }
 
--- app/src/main/java/com/example/greedygoose/foreground/FloatingService.kt
+++ app/src/main/java/com/example/greedygoose/foreground/FloatingService.kt
@@ -27,10 +27,6 @@ class FloatingService : Service() {
     lateinit var floatingGoose : FloatingComponent
     lateinit var floatingEgg : FloatingComponent
 
-    var job: Job? = null
-    val scope = MainScope()
-    var first = true
-
     override fun onBind(intent: Intent): IBinder? {
         floatingGoose = FloatingComponent(this@FloatingService)         // construct a floating object
             .setMovementModule {                      // making it responsive
@@ -42,15 +38,19 @@ class FloatingService : Service() {
                 )
             }
             .build()
+        layEggs()
+        return binder
+    }
+
+    private fun layEggs(){
+        var chance = 1
         MainScope().launch{
             while(true) {
                 // use percentage to determine whether to lay an egg
-                val chance = Random().nextInt(10)
-                if(chance < 3 || first){
-                    first = false
-                    val goose_params = floatingGoose.getLocation()
+                if(chance < 3){
                     floatingEgg = FloatingComponent(this@FloatingService)
-                        .setWindowLayoutParams(goose_params!!.x, goose_params.y)
+                        .setImageResource(R.drawable.egg_small)
+                        .setWindowLayoutParams(floatingGoose.getLocation()!!)
                         .setMovementModule {
                             TouchDeleteModule(
                                 it.params,
@@ -60,14 +60,12 @@ class FloatingService : Service() {
                             )
                         }
                         .build()
-                    floatingEgg.windowModule.binding.gooseImg.setImageResource(R.drawable.egg_small)
-                    delay(5000)
                 }
-                else {
-                    delay(5000)
-                }
-            }}
-        return binder
+                delay(5000)
+
+                chance = Random().nextInt(10)
+            }
+        }
     }
 
     override fun onDestroy() {
@@ -76,54 +74,3 @@ class FloatingService : Service() {
         super.onDestroy()
     }
 }
-
-
-
-//class ForegroundService : Service() {
-//    override fun onBind(intent: Intent): IBinder? {
-//        throw UnsupportedOperationException("Not yet implemented")
-//    }
-//
-//    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-//        // create the custom or default notification
-//        // based on the android version
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) startMyOwnForeground() else startForeground(
-//            1,
-//            Notification()
-//        )
-//
-//        // create an instance of Window class
-//        // and display the content on screen
-//        val window = Window(this)
-//        window.open()
-//
-//
-//        return super.onStartCommand(intent, flags, startId)
-//    }
-//
-//    // for android version >=O we need to create
-//    // custom notification stating
-//    // foreground service is running
-//    @RequiresApi(Build.VERSION_CODES.O)
-//    private fun startMyOwnForeground() {
-//        val NOTIFICATION_CHANNEL_ID = "example.permanence"
-//        val channelName = "Background Service"
-//        val chan = NotificationChannel(
-//            NOTIFICATION_CHANNEL_ID,
-//            channelName,
-//            NotificationManager.IMPORTANCE_MIN
-//        )
-//        val manager = (getSystemService(NOTIFICATION_SERVICE) as NotificationManager)
-//        manager.createNotificationChannel(chan)
-//        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
-//        val notification = notificationBuilder.setOngoing(true)
-//            .setContentTitle("Service running")
-//            .setContentText("Displaying over other apps") // this is important, otherwise the notification will show the way
-//            // you want i.e. it will show some default notification
-//            .setSmallIcon(R.drawable.ic_launcher_foreground)
-//            .setPriority(NotificationManager.IMPORTANCE_MIN)
-//            .setCategory(Notification.CATEGORY_SERVICE)
-//            .build()
-//        startForeground(2, notification)
-//    }
-//}
