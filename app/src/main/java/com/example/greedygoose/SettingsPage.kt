d4990808163dd4e7607bafabeaeacedee898c0c0
Tony Wu <tw.wu@yahoo.com>
2022-06-28 16:36:25 -0400

    refactored conditions to model

--- app/src/main/java/com/example/greedygoose/SettingsPage.kt
+++ app/src/main/java/com/example/greedygoose/SettingsPage.kt
@@ -19,40 +19,50 @@ class SettingsPage : AppCompatActivity() {
 
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
-        if (!is_tie_unlocked) {
+        if (!mod.is_tie_unlocked) {
             binding.customizationOption1.setBackgroundResource(R.drawable.grey_tie)
             binding.customizationOption1.text = "50"
             binding.customizationOption1.textAlignment = View.TEXT_ALIGNMENT_CENTER
             binding.customizationOption1.setTextColor(Color.WHITE)
             binding.customizationOption1.setTextSize(35F)
         }
-        if (!is_goggle_unlocked) {
+        if (!mod.is_goggle_unlocked) {
             binding.customizationOption2.setBackgroundResource(R.drawable.grey_goggles)
             binding.customizationOption2.text = "50"
             binding.customizationOption2.textAlignment = View.TEXT_ALIGNMENT_CENTER
             binding.customizationOption2.setTextColor(Color.WHITE)
             binding.customizationOption2.setTextSize(35F)
         }
-        if (!is_hard_hat_unlocked) {
+        if (!mod.is_hard_hat_unlocked) {
             binding.customizationOption3.setBackgroundResource(R.drawable.grey_hardhat)
-            binding.customizationOption3.text = "50"
+            binding.customizationOption3.text = "53"
             binding.customizationOption3.textAlignment = View.TEXT_ALIGNMENT_CENTER
             binding.customizationOption3.setTextColor(Color.WHITE)
             binding.customizationOption3.setTextSize(35F)
         }
 
+        if (mod.get_theme() === "SCI") {
+            binding.customizationOption2.setBackgroundResource(R.drawable.highlighted_goggles)
+        }
+        else if (mod.get_theme() === "MATH") {
+            binding.customizationOption1.setBackgroundResource(R.drawable.highlighted_tie)
+        }
+        else if (mod.get_theme() === "ENG") {
+            binding.customizationOption3.setBackgroundResource(R.drawable.highlighted_hardhat)
+        }
+
         binding.customizationOption1.setOnClickListener {
-            if (is_tie_unlocked) {
-                if (is_tie_selected) {
-                    is_tie_selected = false
+            if (mod.is_tie_unlocked) {
+                if (mod.is_tie_selected) {
+                    mod.is_tie_selected = false
                     binding.customizationOption1.setBackgroundResource(R.drawable.pink_tie)
-                    current_goose = "goose"
+//                    current_goose = "goose"
                     mod.set_theme("NONE")
                 }
                 else {
-                    is_tie_selected = true
-                    is_goggle_selected = false
-                    is_hard_hat_selected = false
+                    mod.is_tie_selected = true
+                    mod.is_goggle_selected = false
+                    mod.is_hard_hat_selected = false
                     checkGoose()
                 }
             }
@@ -61,17 +71,17 @@ class SettingsPage : AppCompatActivity() {
             }
         }
         binding.customizationOption2.setOnClickListener {
-            if (is_goggle_unlocked) {
-                if (is_goggle_selected) {
-                    is_goggle_selected = false
+            if (mod.is_goggle_unlocked) {
+                if (mod.is_goggle_selected) {
+                    mod.is_goggle_selected = false
                     binding.customizationOption2.setBackgroundResource(R.drawable.science_goggle)
-                    current_goose = "goose"
+//                    current_goose = "goose"
                     mod.set_theme("NONE")
                 }
                 else {
-                    is_tie_selected = false
-                    is_goggle_selected = true
-                    is_hard_hat_selected = false
+                    mod.is_tie_selected = false
+                    mod.is_goggle_selected = true
+                    mod.is_hard_hat_selected = false
                     checkGoose()
                 }
             }
@@ -80,17 +90,17 @@ class SettingsPage : AppCompatActivity() {
             }
         }
         binding.customizationOption3.setOnClickListener {
-            if (is_hard_hat_unlocked) {
-                if (is_hard_hat_selected) {
-                    is_hard_hat_selected = false
+            if (mod.is_hard_hat_unlocked) {
+                if (mod.is_hard_hat_selected) {
+                    mod.is_hard_hat_selected = false
                     binding.customizationOption3.setBackgroundResource(R.drawable.yellow_hard_hat)
-                    current_goose = "goose"
+//                    current_goose = "goose"
                     mod.set_theme("NONE")
                 }
                 else {
-                    is_tie_selected = false
-                    is_goggle_selected = false
-                    is_hard_hat_selected = true
+                    mod.is_tie_selected = false
+                    mod.is_goggle_selected = false
+                    mod.is_hard_hat_selected = true
                     checkGoose()
                 }
             }
@@ -108,55 +118,55 @@ class SettingsPage : AppCompatActivity() {
     }
 
     private fun updateSelectedItem() {
-        if (is_tie_selected) {
+        if (mod.is_tie_selected) {
             binding.customizationOption1.setBackgroundResource(R.drawable.highlighted_tie)
-            current_goose = "pink"
+//            current_goose = "pink"
             mod.set_theme("MATH")
         }
-        else if (is_tie_unlocked) {
+        else if (mod.is_tie_unlocked) {
             binding.customizationOption1.setBackgroundResource(R.drawable.pink_tie)
         }
-        if (is_goggle_selected) {
+        if (mod.is_goggle_selected) {
             binding.customizationOption2.setBackgroundResource(R.drawable.highlighted_goggles)
-            current_goose = "blue"
+//            current_goose = "blue"
             mod.set_theme("SCI")
         }
-        else if (is_goggle_unlocked) {
+        else if (mod.is_goggle_unlocked) {
             binding.customizationOption2.setBackgroundResource(R.drawable.science_goggle)
         }
-        if (is_hard_hat_selected) {
+        if (mod.is_hard_hat_selected) {
             binding.customizationOption3.setBackgroundResource(R.drawable.highlighted_hardhat)
-            current_goose = "yellow"
+//            current_goose = "yellow"
             mod.set_theme("ENG")
         }
-        else if (is_hard_hat_unlocked) {
+        else if (mod.is_hard_hat_unlocked) {
             binding.customizationOption3.setBackgroundResource(R.drawable.yellow_hard_hat)
         }
     }
     private fun updateItem(item_name: String) {
         if (item_name === "pink") {
-            is_tie_unlocked = true
-            is_tie_selected = true
-            is_goggle_selected = false
-            is_hard_hat_selected = false
+            mod.is_tie_unlocked = true
+            mod.is_tie_selected = true
+            mod.is_goggle_selected = false
+            mod.is_hard_hat_selected = false
             binding.customizationOption1.setBackgroundResource(R.drawable.highlighted_tie)
             binding.customizationOption1.text = ""
             updateSelectedItem()
         }
         else if (item_name === "blue") {
-            is_goggle_unlocked = true
-            is_tie_selected = false
-            is_goggle_selected = true
-            is_hard_hat_selected = false
+            mod.is_goggle_unlocked = true
+            mod.is_tie_selected = false
+            mod.is_goggle_selected = true
+            mod.is_hard_hat_selected = false
             binding.customizationOption2.setBackgroundResource(R.drawable.highlighted_goggles)
             binding.customizationOption2.text = ""
             updateSelectedItem()
         }
         else if (item_name === "yellow") {
-            is_hard_hat_unlocked = true
-            is_tie_selected = false
-            is_goggle_selected = false
-            is_hard_hat_selected = true
+            mod.is_hard_hat_unlocked = true
+            mod.is_tie_selected = false
+            mod.is_goggle_selected = false
+            mod.is_hard_hat_selected = true
             binding.customizationOption3.setBackgroundResource(R.drawable.highlighted_hardhat)
             binding.customizationOption3.text = ""
             updateSelectedItem()
@@ -175,13 +185,13 @@ class SettingsPage : AppCompatActivity() {
         updateSelectedItem()
     }
 
-    companion object {
-        var is_tie_unlocked = false
-        var is_goggle_unlocked = false
-        var is_hard_hat_unlocked = true
-        var is_tie_selected = false
-        var is_goggle_selected = false
-        var is_hard_hat_selected = false
-        var current_goose = "goose"
-    }
+//    companion object {
+//        var mod.is_tie_unlocked = true
+//        var mod.is_goggle_unlocked = false
+//        var mod.is_hard_hat_unlocked = false
+//        var mod.is_tie_selected = false
+//        var mod.is_goggle_selected = false
+//        var mod.is_hard_hat_selected = false
+////        var current_goose = "goose"
+//    }
 }
